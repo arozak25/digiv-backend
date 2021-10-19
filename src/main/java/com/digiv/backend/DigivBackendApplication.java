5301e269a5d9c8304cc78c3d9b3acecd0f016865
arozak25 <arozak25@gmail.com>
2021-10-19 14:40:54 +0700

    first commit

---
+++ src/main/java/com/digiv/backend/DigivBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digiv.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigivBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigivBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digiv/backend/DigivBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digiv.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigivBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
