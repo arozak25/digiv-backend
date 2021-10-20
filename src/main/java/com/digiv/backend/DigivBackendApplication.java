0d4e02e329602c1fac9ce6eb1bf48e29e656709c
arozak25 <arozak25@gmail.com>
2021-10-20 14:33:40 +0700

    add entity scan

--- src/main/java/com/digiv/backend/DigivBackendApplication.java
+++ src/main/java/com/digiv/backend/DigivBackendApplication.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EntityScan
+@EntityScan("com.digiv.backend.entity")
 public class DigivBackendApplication {
 
 	public static void main(String[] args) {
