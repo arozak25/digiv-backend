0f42a089fd0cf9e56612472a2a88e0f48546b47c
arozak25 <arozak25@gmail.com>
2021-10-20 14:39:11 +0700

    add enable jpa repositories

--- src/main/java/com/digiv/backend/DigivBackendApplication.java
+++ src/main/java/com/digiv/backend/DigivBackendApplication.java
@@ -3,10 +3,12 @@ package com.digiv.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableJpaRepositories
 @EntityScan("com.digiv.backend.entity")
 public class DigivBackendApplication {
 
